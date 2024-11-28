16b61ab1cadd15cf461a9c3a07cd2174e1a706aa
engchina <atjapan2015@gmail.com>
2024-11-28 19:16:15 +0800

    update

--- activate_env.sh
+++ activate_env.sh
@@ -3,7 +3,7 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 
 # Activate the desired conda environment
-conda activate no.1-rag
+conda activate know-rag-from-scratch
 
 # Start a new shell session in the activated environment
-PS1="(no.1-rag) \u@\h:\w# " bash --noprofile --norc
+PS1="(know-rag-from-scratch) \u@\h:\w# " bash --noprofile --norc
